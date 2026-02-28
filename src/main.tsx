0f7c4575ecfbac48a33775e6006984b717812c00
Abhishek Shandilya <abhi170203@gmail.com>
2026-03-01 00:09:38 +0530

    fix: remove the border (was distorted on the mobile view), change default theme back to light.

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="dark">
+    <ThemeProvider attribute="class" defaultTheme="light">
       <App />
     </ThemeProvider>
   </React.StrictMode>,
